a99f4fd4ebbdcc7794f5698ca7ad326cc28d05b0
Ramon Castan <rcastan@br.ibm.com>
2017-07-07 11:15:39 -0300

    first part complete

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,23 +4,26 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
 
-  changeTitle(title) {
-    this.setState({title});
-  }
+    constructor(){
+        super();
+        this.state = {
+            title:"Welcome"
+        }
+    }
 
-  render() {
-    return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
-      </div>
+    changeTitle(title){
+        this.setState({title: title});
+    }
+
+    render() {
+        
+        return (
+        <div>
+            <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+            <Footer />
+        </div>
     );
-  }
+}   
+
 }
